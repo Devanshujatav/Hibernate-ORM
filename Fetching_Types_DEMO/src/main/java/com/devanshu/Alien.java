5ea0f95b14c0712818271a16befb35529053ba8f
Devanshu Jatav <devanshujatav2003@gmail.com>
2025-08-22 06:45:32 +0530

    Relatinoal Mapping Practical

---
+++ Fetching_Types_DEMO/src/main/java/com/devanshu/Alien.java
@@ -0,0 +1,12 @@
+package com.devanshu;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Alien {
+    @Id
+    private int aid;
+    private int anem
+}
